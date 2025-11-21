408efcb76466006e973f83a0ba610f61cdc73ca5
emma <emmakdalmeida@gmail.com>
2025-11-21 10:05:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bato-tibo',
+  webDir: 'www'
+};
+
+export default config;
