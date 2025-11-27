a7daf0d567cbc667614a8d2207d55d8760fb4c45
GreyzRox <greyzrox@gmail.com>
2025-11-27 18:58:53 +0100

    ajout debut login (non fonctionnel)

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonButton, IonIcon, IonImg } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 import { TiboHeaderComponent } from '../tibo-header/tibo-header.component';
 import { addIcons } from 'ionicons';
 import { callOutline, mailOutline, logoFacebook, mail } from 'ionicons/icons';
@@ -11,7 +12,7 @@ import { callOutline, mailOutline, logoFacebook, mail } from 'ionicons/icons';
   templateUrl: './contact.page.html',
   styleUrls: ['./contact.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, IonButton, IonIcon, TiboHeaderComponent, IonImg]
+  imports: [IonContent, CommonModule, FormsModule, IonButton, IonIcon, TiboHeaderComponent, IonImg, RouterModule]
 })
 export class ContactPage implements OnInit {
 
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, input, OnInit } from '@angular/core';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonImg} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonImg]
 })
 export class LoginPage implements OnInit {
 
