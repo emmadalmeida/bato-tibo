c40280c4752e18605380ee68971b7a0671c58a6f
GreyzRox <greyzrox@gmail.com>
2025-11-27 10:02:45 +0100

    header partout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet, Platform } from '@ionic/angular/standalone';
+import { TiboHeaderComponent } from './tibo-header/tibo-header.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, TiboHeaderComponent],
 })
 export class AppComponent {
   constructor() {}
