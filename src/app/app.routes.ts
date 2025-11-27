edbecfab081c40af3b448934fa043d1094138426
GreyzRox <greyzrox@gmail.com>
2025-11-27 20:56:41 +0100

    page login fonctionnel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -44,6 +44,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'signup',
+    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
 
 
 ];
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,13 @@
 import { Component, input, OnInit } from '@angular/core';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonImg} from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonImg]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonImg, RouterModule]
 })
 export class LoginPage implements OnInit {
 
