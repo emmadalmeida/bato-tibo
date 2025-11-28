3507c248a5ae1bdc17d8cde6770eb608af43f5a8
emma <emmakdalmeida@gmail.com>
2025-11-28 15:42:48 +0100

    spin bouton log out

--- src/app/tibo-header/tibo-header.component.ts
+++ src/app/tibo-header/tibo-header.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonButton, IonIcon, IonHeader, IonToolbar, IonImg } from '@ionic/angular/standalone';
-import { Router } from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tibo-header',
   templateUrl: './tibo-header.component.html',
   styleUrls: ['./tibo-header.component.scss'],
   standalone: true,
-  imports: [IonButton, IonIcon, IonHeader, IonToolbar, IonImg],
+  imports: [IonButton, IonIcon, IonHeader, IonToolbar, IonImg, RouterLink],
 })
 export class TiboHeaderComponent implements OnInit {
 
@@ -23,5 +23,9 @@ export class TiboHeaderComponent implements OnInit {
 
   onLoadAccueil() {
     this.router.navigate(['tabs/tab1']);
+  }
+
+  openSpin() {
+    this.router.navigate(['/logo-spin'])
   }
 }
